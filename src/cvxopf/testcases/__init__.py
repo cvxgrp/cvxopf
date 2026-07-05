c3354dce7104ad213d3cce7b16410a5d48a1856d
Bennet Meyers <bennet.meyersim@nrel.gov>
2026-07-04 22:03:10 -0700

    update documentation

--- src/cvxopf/testcases/__init__.py
+++ src/cvxopf/testcases/__init__.py
@@ -1,5 +1,17 @@
 """
 Built-in MATPOWER-format test cases.
+
+Available test cases
+--------------------
+case9 : 9-bus, 3-generator system (Chow, p. 70)
+case14 : IEEE 14-bus test case
+
+Usage
+-----
+    from cvxopf.testcases import case9, case14
+
+    ppc = case9()
+    ppc = case14()
 """
 
 from cvxopf.testcases.case9 import case9
